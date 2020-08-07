e4e0ce9efc28de58e7affd72edcaeaf44ccc442b
prodigyuy <68932125+prodigyuy@users.noreply.github.com>
2020-08-07 10:45:14 -0300

    Entrega 1

--- js/login.js
+++ js/login.js
@@ -1,3 +1,34 @@
+let loginButton = document.getElementById("login");
+let inputEmail = document.getElementById("inputEmail");
+let inputPassword = document.getElementById("inputPassword");
+
+let prueba = null;
+let expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+
+function beforeLogin(){
+
+    prueba = inputEmail.value;
+    let esValido = expReg.test(prueba);
+   
+   if(esValido && inputPassword.value){
+
+    window.location.href = "inicio.html";
+
+    return false;
+
+   }else{
+       alert("El formato de correo o la contraseña no son validos.");
+   }
+
+ 
+    
+}
+
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
